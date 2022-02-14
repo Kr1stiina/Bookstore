ca2531ae46a2779ecce4b1e12f9f2e155c137df1
Kr1stiina <kristiina.johansson@hotmail.fi>
2022-02-15 01:23:53 +0200

    Added Create and Delete functionalities

--- src/main/java/hh/Bookstore/web/BookController.java
+++ src/main/java/hh/Bookstore/web/BookController.java
@@ -3,9 +3,12 @@ package hh.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
+import hh.Bookstore.domain.Book;
 import hh.Bookstore.domain.BookRepository;
 
 
@@ -22,7 +25,22 @@ public class BookController {
 	model.addAttribute("books", bookRepository.findAll());
 	return "index";
 	}
-
-
+	
+    @RequestMapping(value = "/add")
+    public String addStudent(Model model){
+    	model.addAttribute("book", new Book());
+        return "addBook";
+    } 
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        bookRepository.save(book);
+        return "redirect:index";
+    }
+    
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model)
+    { bookRepository.deleteById(bookId);
+    return "redirect:../index";
+   }
 
 }
